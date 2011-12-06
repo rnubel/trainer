9c92607d10653f1ee0418c890abcc25cb717196c
Robert Nubel <robert.nubel@gmail.com>
2011-12-06 15:08:03 -0600

    Refactors to use factory.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,7 +2,9 @@ require 'spec/spec_helper'
 
 describe Region do
   it "should belong to a Song" do
-    Region.new.should respond_to :song
+    r = Factory.create(:region)
+    
+    r.should respond_to :song
   end
 end
 
