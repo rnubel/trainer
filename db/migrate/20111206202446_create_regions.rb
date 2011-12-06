cf83b584c7950e41b68b9f6078dc3955eedba303
Robert Nubel <robert.nubel@gmail.com>
2011-12-06 14:51:09 -0600

    Fixes migration typo.

--- db/migrate/20111206202446_create_regions.rb
+++ db/migrate/20111206202446_create_regions.rb
@@ -3,7 +3,7 @@ class CreateRegions < ActiveRecord::Migration
     create_table :regions do |t|
       t.float :start
       t.float :end
-      t.int :song_id
+      t.integer :song_id
 
       t.timestamps
     end
