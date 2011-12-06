dbfd8f48e41594e942ab3e6fc19923f55de8e98f
Robert Nubel <robert.nubel@gmail.com>
2011-12-06 15:43:51 -0600

    Stubs out scaffold for views.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,17 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.find :all
+  end
+
+  def show
+    @song = Song.find params[:id]
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create params[:song]
+  end
 end
